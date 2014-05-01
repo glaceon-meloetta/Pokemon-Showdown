eef970372cdd38ad89a44e78c4b0ab26fae81f4c
glaceon-meloetta <mikekraats@hotmail.com>
2014-05-01 20:31:30 +0200

    Create pokedex.js

---
+++ mods/nohax/pokedex.js
@@ -0,0 +1,197 @@
+exports.BattlePokedex = {
+	butterfree: {
+		inherit: true,
+		baseStats: {hp:60,atk:45,def:50,spa:80,spd:80,spe:70}
+	},
+	beedrill: {
+		inherit: true,
+		baseStats: {hp:65,atk:80,def:40,spa:45,spd:80,spe:75}
+	},
+	pikachu: {
+		inherit: true,
+		baseStats: {hp:35,atk:55,def:30,spa:50,spd:40,spe:90}
+	},
+	raichu: {
+		inherit: true,
+		baseStats: {hp:60,atk:90,def:55,spa:90,spd:80,spe:100}
+	},
+	clefairy: {
+		inherit: true,
+		types:["Normal"]
+	},
+	clefable: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:95,atk:70,def:73,spa:85,spd:90,spe:60}
+	},
+	jigglypuff: {
+		inherit: true,
+		types:["Normal"],
+		abilities: {0:"Cute Charm",H:"Friend Guard"}
+	},
+	wigglytuff: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:140,atk:70,def:45,spa:75,spd:50,spe:45},
+		abilities: {0:"Cute Charm",H:"Frisk"}
+	},
+	alakazam: {
+		inherit: true,
+		baseStats: {hp:55,atk:50,def:45,spa:135,spd:85,spe:120}
+	},
+	golem: {
+		inherit: true,
+		baseStats: {hp:80,atk:110,def:130,spa:55,spd:65,spe:45}
+	},
+	mrmime: {
+		inherit: true,
+		types:["Psychic"]
+	},
+	igglybuff: {
+		inherit: true,
+		types:["Normal"],
+		abilities: {0:"Cute Charm",H:"Friend Guard"}
+	},
+	togepi: {
+		inherit: true,
+		types:["Normal"]
+	},
+	togetic: {
+		inherit: true,
+		types:["Normal","Flying"]
+	},
+	cleffa: {
+		inherit: true,
+		types:["Normal"]
+	},
+	marill: {
+		inherit: true,
+		types:["Water"]
+	},
+	azumarill: {
+		inherit: true,
+		types:["Water"],
+		baseStats: {hp:100,atk:50,def:80,spa:50,spd:80,spe:50}
+	},
+	snubbull: {
+		inherit: true,
+		types:["Normal"]
+	},
+	granbull: {
+		inherit: true,
+		types:["Normal"]
+	},
+	ralts: {
+		inherit: true,
+		types:["Psychic"]
+	},
+	kirlia: {
+		inherit: true,
+		types:["Psychic"]
+	},
+	gardevoir: {
+		inherit: true,
+		types:["Psychic"]
+	},
+	azurill: {
+		inherit: true,
+		types:["Normal"]
+	},
+	mawile: {
+		inherit: true,
+		types:["Steel"]
+	},
+	plusle: {
+		inherit: true,
+		abilities: {0:"Plus"}
+	},
+	minun: {
+		inherit: true,
+		abilities: {0:"Minus"}
+	},
+	kecleon: {
+		inherit: true,
+		abilities: {0:"Color Change"}
+	},
+	milotic: {
+		inherit: true,
+		abilities: {0:"Marvel Scale",H:"Cute Charm"}
+	},
+	duskull: {
+		inherit: true,
+		abilities: {0:"Levitate"}
+	},
+	dusclops: {
+		inherit: true,
+		abilities: {0:"Pressure"}
+	},
+	starly: {
+		inherit: true,
+		abilities: {0:"Keen Eye"}
+	},
+	staraptor: {
+		inherit: true,
+		baseStats: {hp:85,atk:120,def:70,spa:50,spd:50,spe:100}
+	},
+	mimejr: {
+		inherit: true,
+		types:["Psychic"]
+	},
+	togekiss: {
+		inherit: true,
+		types:["Normal","Flying"]
+	},
+	dusknoir: {
+		inherit: true,
+		abilities: {0:"Pressure"}
+	},
+	venipede: {
+		inherit: true,
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	whirlipede: {
+		inherit: true,
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	scolipede: {
+		inherit: true,
+		baseStats: {hp:60,atk:90,def:89,spa:55,spd:69,spe:112},
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	whimsicott: {
+		inherit: true,
+		types:["Grass"]
+	},
+	gothita: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	gothorita: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	gothitelle: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	ferroseed: {
+		inherit: true,
+		abilities: {0:"Iron Barbs"}
+	},
+	ferrothorn: {
+		inherit: true,
+		abilities: {0:"Iron Barbs"}
+	},
+	litwick: {
+		inherit: true,
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	},
+	lampent: {
+		inherit: true,
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	},
+	chandelure: {
+		inherit: true,
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	}
+};
