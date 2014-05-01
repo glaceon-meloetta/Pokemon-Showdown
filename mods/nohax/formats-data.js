f4dcea104fd8d790f8c7e459faf456d78ddc5276
glaceon-meloetta <mikekraats@hotmail.com>
2014-05-01 20:30:52 +0200

    Create formats-data.js

---
+++ mods/nohax/formats-data.js
@@ -0,0 +1,21 @@
+exports.BattleFormatsData = {
+zapdos: {
+viableMoves: {"thunderbolt":1,"heatwave":1,"hiddenpowergrass":1,"hiddenpowerice":1,"roost":1,"toxic":1,"substitute":1},
+eventPokemon: [
+{"generation":3,"level":70,"moves":["agility","detect","drillpeck","charge"]},
+{"generation":3,"level":50,"moves":["thunderbolt","extrasensory","batonpass","metalsound"]}
+],
+dreamWorldRelease: true,
+tier: "UU"
+},
+shayminsky: {
+viableMoves: {"seedflare":1,"earthpower":1,"airslash":1,"hiddenpowerice":1,"hiddenpowerfire":1,"substitute":1,"leechseed":1},
+eventPokemon: [
+{"generation":4,"level":50,"moves":["seedflare","aromatherapy","substitute","energyball"],"pokeball":"cherishball"},
+{"generation":4,"level":30,"moves":["synthesis","leechseed","magicalleaf","growth"]},
+{"generation":4,"level":30,"moves":["growth","magicalleaf","leechseed","synthesis"]},
+{"generation":5,"level":50,"moves":["seedflare","leechseed","synthesis","sweetscent"],"pokeball":"cherishball"}
+],
+tier: "OU"
+},
+};
